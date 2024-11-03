bfa17773bbaad1c03b26d736001ed22dda77d453
taiga-shiokawa <taiga.hr12@gmail.com>
2024-11-03 20:58:24 +0900

    メモリストスタイリング完了

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,43 +3,21 @@ import { View, Text, StyleSheet } from "react-native";
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-       {/* Header View */}
-      <View>
-        <View>
-          <Text>AI Memo App</Text>
-          <Text>logout</Text>
+      {/* Header View */}
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>AI Memo App</Text>
+          <Text style={styles.headerRight}>logout</Text>
         </View>
       </View>
 
       {/* Memo List View */}
       <View>
         {/* Memo Item View */}
-        <View>
+        <View style={styles.memoListItem}>
           <View>
-            <Text>Test List</Text>
-            <Text>2024年11月3日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        {/* Memo Item View */}
-        <View>
-          <View>
-            <Text>Test List</Text>
-            <Text>2024年11月3日 10:00</Text>
-          </View>
-          <View>
-            <Text>X</Text>
-          </View>
-        </View>
-
-        {/* Memo Item View */}
-        <View>
-          <View>
-            <Text>Test List</Text>
-            <Text>2024年11月3日 10:00</Text>
+            <Text style={styles.memoListItemTitle}>Test List</Text>
+            <Text style={styles.memoListItemDate}>2024年11月3日 10:00</Text>
           </View>
           <View>
             <Text>X</Text>
@@ -49,18 +27,81 @@ const Index = (): JSX.Element => {
       </View>
 
       {/* Memo Add Button */}
-      <View>
-        <Text>＋</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>＋</Text>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  // 画面全体
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#ffffff",
+  },
+  // ヘッダー
+  header: {
+    backgroundColor: "#467FD3",
+    height: 104,
+    justifyContent: "flex-end",
+  },
+  headerInner: {
+    alignItems: "center",
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: "bold",
+    color: "#ffffff",
+  },
+  headerRight: {
+    position: "absolute",
+    right: 16,
+    bottom: 16,
+    color: "rgba(255, 255, 255, 0.7)",
+  },
+  // メモリスト
+  memoListItem: {
+    backgroundColor: "#ffffff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 16,
+    paddingHorizontal: 19,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.15)",
+  },
+  memoListItemTitle: {
+    fontSize: 16,
+    lineHeight: 32,
+  },
+  memoListItemDate: {
+    fontSize: 12, 
+    lineHeight: 16,
+    color: "#848484",
+  },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467FD3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: {width: 0, height: 8},
+    elevation: 8, // ここはAndroid用
+  },
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 48,
   },
 });
 
