d9e4b7f2b9011206b92f8e651389c5078eab1e7f
taiga-shiokawa <taiga.hr12@gmail.com>
2024-11-03 20:22:10 +0900

    メモアイテムページ追加

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,67 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 const Index = (): JSX.Element => {
   return (
-    <View>
-      <Text>Hello World!!</Text>
+    <View style={styles.container}>
+       {/* Header View */}
+      <View>
+        <View>
+          <Text>AI Memo App</Text>
+          <Text>logout</Text>
+        </View>
+      </View>
+
+      {/* Memo List View */}
+      <View>
+        {/* Memo Item View */}
+        <View>
+          <View>
+            <Text>Test List</Text>
+            <Text>2024年11月3日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* Memo Item View */}
+        <View>
+          <View>
+            <Text>Test List</Text>
+            <Text>2024年11月3日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        {/* Memo Item View */}
+        <View>
+          <View>
+            <Text>Test List</Text>
+            <Text>2024年11月3日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      {/* Memo Add Button */}
+      <View>
+        <Text>＋</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Index;
